5e68ae4634eb627bf6c48701bd1514b22ce8709e
Victor Katyshev <vickatyshev@yahoo.com>
2019-08-08 10:01:11 -0400

    New Test 1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React! You dumb fuck! do not waste time!
+          Learn React! You dumb fuck! Do not waste time!
         </a>
       </header>
     </div>
