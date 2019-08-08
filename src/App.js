76e0b59bb78d994d60f2a865ae0b0381b889f771
Victor <vk@MacAir.home>
2019-08-08 09:55:02 -0400

    New Test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React! You dumb fuck! do not waste time!
         </a>
       </header>
     </div>
